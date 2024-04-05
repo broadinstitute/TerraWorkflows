4449773c04df35147210515030594dcc6b93e919
bahill <bhill@broadinstitute.org>
2024-04-05 12:52:28 -0400

    fixing df formatting, adding export to CSV and stub for conversion to pdf

--- pipelines/cohort_screening/reporting/json_to_pdf.py
+++ pipelines/cohort_screening/reporting/json_to_pdf.py
@@ -1,3 +1,8 @@
+
+# This script generates an HTML report from a JSON file containing genomic variant data.
+import pandas as pd
+
+# dummy data
 sample_identifier = "Sample 1"
 
 variants = [
@@ -15,11 +20,42 @@ variants = [
         "ClinVar classification": "ClinVar classification 1",
         "ClinVar phenotypes": "ClinVar phenotypes 1",
     },
+    {
+        "Gene": "Gene 2",
+        "contig": "contig 2",
+        "position": "position 2",
+        "Ref allele": "Ref allele 2",
+        "Alt Allele": "Alt Allele 2",
+        "dbSNP": "dbSNP 2",
+        "Zygosity": "Zygosity 2",
+        "consequence": "consequence 2",
+        "Protein change": "Protein change 2",
+        "gnomAD AF": "gnomAD AF 2",
+        "ClinVar classification": "ClinVar classification 2",
+        "ClinVar phenotypes": "ClinVar phenotypes 2",
+    },
     # Add more variants as needed
 ]
+# Convert the list of variant dictionaries to a DataFrame
+# TODO: create a for loop to iterate over the variants and *append* them to the DataFrame
+# df_var = df_var.append(data, ignore_index=True)
+df_var = pd.DataFrame(variants)
 
-genes = ["ABCC8", "APPL1", "BLK", "CEL", "GCK", "HNF1B", "HNF4A", "INS", "KCNJ11", "NEUROD1", "PAX4", "PDX1"]
+# Apply styles to the DataFrame
+borders = [{'selector': 'td, th, table',
+            'props': [('border', '1px solid lightgrey'),
+                      ('border-collapse', 'collapse')]
+            }]
+# Convert the DataFrame to an HTML table
+df_var_html = df_var.style.hide().set_table_styles(borders).to_html()
 
+# MOBY (Maturity-Onset Diabetes of the Young) genes
+# obtained from Naylor 2018, https://www.ncbi.nlm.nih.gov/books/NBK500456/#mody-ov.Genetic_Causes_of_MODY
+# TODO: Add FAQ#2 - unclear if this is part of the report or just a comment
+# see #m42-pipelines slack
+mody_genes = ["ABCC8", "APPL1", "BLK", "CEL", "GCK", "HNF1B", "HNF4A", "INS", "KCNJ11", "NEUROD1", "PAX4", "PDX1"]
+
+# Generate HTML report
 html_content = f"""
 <!DOCTYPE html>
 <html>
@@ -35,37 +71,16 @@ html_content = f"""
         <li>This report is not meant for direct disclosure to patients.</li>
         <li>This report does not mean that the person with these variants has diabetes.</li>
         <li>Clinicians should confirm this result before using it as part of clinical care.</li>
-    <h2>We have identified the following variants that warrant further investigation:</h2>
-    <table>
-        <tr>
-            <th>Gene</th>
-            <th>contig</th>
-            <th>position</th>
-            <th>Ref allele</th>
-            <th>Alt Allele</th>
-            <th>dbSNP</th>
-            <th>Zygosity</th>
-            <th>consequence</th>
-            <th>Protein change</th>
-            <th>gnomAD AF</th>
-            <th>ClinVar classification</th>
-            <th>ClinVar phenotypes</th>
-        </tr>
+    </ul>
+    <p>We have identified the following variants that warrant further investigation:</p>
+    {df_var_html}
 """
-
-for variant in variants:
-    html_content += "<tr>"
-    for key, value in variant.items():
-        html_content += f"<td>{value}</td>"
-    html_content += "</tr>"
-
 html_content += """
-    </table>
     <h2>We have examined the following genes, which have been implicated in Maturity-Onset Diabetes of the Young (MODY):</h2>
     <ul>
 """
 
-for gene in genes:
+for gene in mody_genes:
     html_content += f"<li>{gene}</li>"
 
 html_content += """
@@ -75,5 +90,14 @@ html_content += """
 </html>
 """
 
+# write out the HTML content to a file
 with open('report.html', 'w') as html_file:
     html_file.write(html_content)
+print("HTML report generated successfully!")
+
+# TODO: Add code to convert the HTML report to a PDF file
+
+
+# Export variants dataframe as tsv
+df_var.to_csv('variants.tsv', sep='\t', index=False)
+print("Variants exported successfully!")
